0c2eece3130df9b0827e61a2a638ae304973e505
Jorawar Singh Nijjar <kawal5_nijjar@yahoo.co.in>
2020-11-07 18:16:23 -0500

    Updates

---
+++ precache-manifest.ce03ddfbb7f956eac1b84033791f1c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab184898448a73f033856219377caf9",
+    "url": "/team-dragon/index.html"
+  },
+  {
+    "revision": "5688cfe1882bff736227",
+    "url": "/team-dragon/static/css/main.ace0e643.chunk.css"
+  },
+  {
+    "revision": "73fa76ec1fac9fa958ef",
+    "url": "/team-dragon/static/js/2.42e076a5.chunk.js"
+  },
+  {
+    "revision": "fd8c3014c12221a578603fb1a4a9424f",
+    "url": "/team-dragon/static/js/2.42e076a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5688cfe1882bff736227",
+    "url": "/team-dragon/static/js/main.c5d9d121.chunk.js"
+  },
+  {
+    "revision": "dadc82d0cbe9d19fd127",
+    "url": "/team-dragon/static/js/runtime-main.8d32baac.js"
+  }
+]);
